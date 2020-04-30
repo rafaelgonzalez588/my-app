ec362f00a36cf6788ca999753a471ce8b7ed3ad4
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-04-30 18:50:16 -0500

    corrección.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-        <h1>Esta es mi primera aplicación</h1>
+        <h1>Mi primera aplicación</h1>
         <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
