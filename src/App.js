6b3cf10cec1639e4d15177698170be30ab7b3362
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-04-29 19:39:03 -0500

    creacion de app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Esta es mi primera aplicación en React, está padrísimo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </h1>
     </div>
   );
 }
